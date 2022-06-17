28d3188563d88aef0f5abc8c2a26327ff2eecd27
geraud <geraud.bertrand@gmail.com>
2022-06-17 09:45:01 +0200

    Routes php

--- components/menu.php
+++ components/menu.php
@@ -1,12 +1,14 @@
+<?php $ROOT = "/~gommettes/Seminaire/" ?>
+
 <html> 
 	<head>
 		<title>Classe de CE1</title>
-		<link rel="icon" href="/Seminaire/Content/img/favico.png" />
-		<link href="/Seminaire/Content/css/custom.css" rel="stylesheet">
-		<link href="/Seminaire/Content/css/all.css" rel="stylesheet">
-		<script src="/Seminaire/Content/js/popper.min.js"></script>
-		<script src="/Seminaire/Content/js/jquery.min.js"></script>
-		<script src="/Seminaire/Content/js/bootstrap.min.js"></script>
+		<link rel="icon" href="<?=$ROOT?>Content/img/favico.png" />
+		<link href="<?=$ROOT?>Content/css/custom.css" rel="stylesheet">
+		<link href="<?=$ROOT?>Content/css/all.css" rel="stylesheet">
+		<script src="<?=$ROOT?>Content/js/popper.min.js"></script>
+		<script src="<?=$ROOT?>Content/js/jquery.min.js"></script>
+		<script src="<?=$ROOT?>Content/js/bootstrap.min.js"></script>
 		<meta http-equiv="refresh" content="300" />
 	</head>
 	
@@ -26,33 +28,33 @@
 				<div class="btn-group" role="group">
 					<button id="btnGroupDrop1" type="button" class="menu btn btn-danger dropdown-toggle font-weight-bold fa fa-book-open" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><b style="font-family:perso"><br> Français</b></button>
 					<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<a class="dropdown-item" href="/Seminaire/cours/francais/lecons/francais_lecons.php">Leçons</a>
-						<a class="dropdown-item" href="/Seminaire/cours/dictee/francais_dictees.php">Dictées</a>
-						<a class="dropdown-item" href="/Seminaire/jeux/francais/francais_jeux.php">Jeux</a>
+						<a class="dropdown-item" href="<?=$ROOT?>cours/francais/lecons/francais_lecons.php">Leçons</a>
+						<a class="dropdown-item" href="<?=$ROOT?>cours/dictee/francais_dictees.php">Dictées</a>
+						<a class="dropdown-item" href="<?=$ROOT?>jeux/francais/francais_jeux.php">Jeux</a>
 					</div>
 				</div>
 				
 				<div class="btn-group" role="group">
 					<button id="btnGroupDrop1" type="button" class="menu btn btn-info dropdown-toggle font-weight-bold fa fa-plus" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><b style="font-family:perso"><br> Mathématiques</b></button>
 					<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<a class="dropdown-item" href="/Seminaire/cours/mathematiques/mathematiques_lecons.php">Leçons</a>
-						<a class="dropdown-item" href="/Seminaire/jeux/mathematiques/mathematiques_jeux.php">Jeux</a>
+						<a class="dropdown-item" href="<?=$ROOT?>cours/mathematiques/mathematiques_lecons.php">Leçons</a>
+						<a class="dropdown-item" href="<?=$ROOT?>jeux/mathematiques/mathematiques_jeux.php">Jeux</a>
 					</div>
 				</div>
 				
 				<div class="btn-group" role="group">
 					<button id="btnGroupDrop1" type="button" class="menu btn btn-primary dropdown-toggle font-weight-bold fas fa-globe-americas" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><b style="font-family:perso"><br> Anglais</b></button>
 					<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<a class="dropdown-item" href="/Seminaire/cours/anglais/anglais_lecons.php">Leçons</a>
-						<a class="dropdown-item" href="/Seminaire/jeux/anglais/anglais_jeux.php">Jeux</a>					
+						<a class="dropdown-item" href="<?=$ROOT?>cours/anglais/anglais_lecons.php">Leçons</a>
+						<a class="dropdown-item" href="<?=$ROOT?>jeux/anglais/anglais_jeux.php">Jeux</a>					
 					</div>
 				</div>
 				
 				<div class="btn-group" role="group">
 					<button id="btnGroupDrop1" type="button" class="menu btn btn-success dropdown-toggle font-weight-bold fas fa-rocket" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><b style="font-family:perso"><br> Découverte du monde</b></button>
 					<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<a class="dropdown-item" href="/Seminaire/cours/ddm/ddm_lecons.php">Leçons</a>
-						<a class="dropdown-item" href="/Seminaire/jeux/ddm/ddm_jeux.php">Jeux</a>					
+						<a class="dropdown-item" href="<?=$ROOT?>cours/ddm/ddm_lecons.php">Leçons</a>
+						<a class="dropdown-item" href="<?=$ROOT?>jeux/ddm/ddm_jeux.php">Jeux</a>					
 					</div>
 				</div>
 				<a href="/Seminaire/cours/arts/chorale.php" class="menu btn btn-warning font-weight-bold fa fa-music">
@@ -63,11 +65,11 @@
 						<b style="font-family:perso"><br> Classe</b>
 					</button>				
 					<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<a class="dropdown-item" href="/Seminaire/classe/rituel.php">Devoirs</a>
-						<a class="dropdown-item" href="/Seminaire/classe/quadrillage.php">Quadrillage</a>
-						<a class="dropdown-item" href="/Seminaire/classe/ecriture.php">Ecriture</a>
-						<a class="dropdown-item" href="/Seminaire/classe/tbi.php">TBI</a>
-						<a class="dropdown-item" href="/Seminaire/photos/photos.php">Photos</a>
+						<a class="dropdown-item" href="<?=$ROOT?>classe/rituel.php">Devoirs</a>
+						<a class="dropdown-item" href="<?=$ROOT?>classe/quadrillage.php">Quadrillage</a>
+						<a class="dropdown-item" href="<?=$ROOT?>classe/ecriture.php">Ecriture</a>
+						<a class="dropdown-item" href="<?=$ROOT?>classe/tbi.php">TBI</a>
+						<a class="dropdown-item" href="<?=$ROOT?>photos/photos.php">Photos</a>
 					</div>
 				</div>
 				<a href="javascript:history.go(-1)" class="menu btn btn-dark font-weight-bold fa fa-arrow-circle-left">
